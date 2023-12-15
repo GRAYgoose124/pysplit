715a19426c4f100f9b1179f2c5e840526b7cec57
Grayson Miller <grayson.miller124@gmail.com>
2023-12-15 16:43:23 -0700

    fixes - ast for main_block

--- monosplit/split.py
+++ monosplit/split.py
@@ -2,24 +2,53 @@ import re
 import ast
 
 
-def detect_main_block(lines):
-    """Detect the main block of code in a file, whether it's in a function or if statement.
-
-    Then return the whole block, including the if statement or def main .
+def detect_main_block(lines) -> list[str]:
     """
-    main_block = []
-    in_main_block = False
-    for line in lines:
-        if in_main_block:
-            main_block.append(line)
-        elif re.match(r"if __name__ == \"__main__\":", line):
-            in_main_block = True
-            main_block.append(line)
-        elif re.match(r"def main\(\):", line):
-            in_main_block = True
-            main_block.append(line)
-    if main_block:
-        return main_block
+    Detect the main block of code in a source string, whether it's in a function or if statement.
+    Then return the whole block, including the if statement or def main.
+    """
+
+    class MainBlockVisitor(ast.NodeVisitor):
+        def __init__(self):
+            self.main_block = None
+            self.start_lineno = None
+            self.end_lineno = None
+
+        def visit_If(self, node):
+            # Check if the node is 'if __name__ == "__main__":'
+            if (
+                isinstance(node.test, ast.Compare)
+                and isinstance(node.test.left, ast.Name)
+                and node.test.left.id == "__name__"
+                and isinstance(node.test.comparators[0], ast.Str)
+                and node.test.comparators[0].s == "__main__"
+            ):
+                self.start_lineno = node.lineno
+                self.end_lineno = node.end_lineno
+                self.main_block = ast.get_source_segment(source, node)
+            # Continue traversing child nodes
+            self.generic_visit(node)
+
+        def visit_FunctionDef(self, node):
+            # Check if the node is 'def main():'
+            if node.name == "main":
+                self.start_lineno = node.lineno
+                self.end_lineno = node.end_lineno
+                self.main_block = ast.get_source_segment(source, node)
+            # Continue traversing child nodes
+            self.generic_visit(node)
+
+    source = "\n".join(lines)
+    tree = ast.parse(source)
+    visitor = MainBlockVisitor()
+    visitor.visit(tree)
+
+    if visitor.main_block:
+        return (visitor.start_lineno, visitor.end_lineno), visitor.main_block.split(
+            "\n"
+        )
+    else:
+        return None, None
 
 
 def extract_exports(lines):
@@ -39,7 +68,7 @@ def parse_imports(line):
 
 
 def parse_body_for_used_ports(lines, ports):
-    tree = ast.parse("".join(lines))
+    tree = ast.parse("\n".join(lines))
     for node in ast.walk(tree):
         if isinstance(node, ast.Name):
             if node.id in ports:
@@ -100,29 +129,8 @@ def split_file_into_module(filename):
     if current_file:
         exports[current_file] = extract_exports(file_contents[current_file])
 
-    # create the new files
-    for new_filename, contents in file_contents.items():
-        used_exports = exports[new_filename]
-        used_imports = parse_body_for_used_ports(contents, imports)
-
-        with open(new_filename, "w") as new_file:
-            for name in used_imports:
-                if name in imports:
-                    new_file.write(imports[name])
-            if used_exports:
-                new_file.write(f"\n__all__ = {used_exports}\n\n\n")
-            new_file.writelines(contents)
-    created_filenames = list(file_contents.keys())
-
-    # Update the __init__ file to re-export symbols from the new module directory like the old file.
-    with open("__init__.py", "w") as file:
-        for new_filename, used_exports in exports.items():
-            if used_exports:
-                module_name = re.sub(r"\.py$", "", new_filename)
-                file.write(f"from .{module_name} import *\n")
-
-        file.write(f"\n__all__ = {[x for y in exports.values() for x in y]}\n")
-    created_filenames.append("__init__.py")
+    # build files
+    created_filenames = []
 
     # Detect the main block and create a __main__.py file with the appropriate module imports
     up_to_first_pragma = [
@@ -130,7 +138,8 @@ def split_file_into_module(filename):
         for line in up_to_first_pragma
         if not (line.startswith("import ") or line.startswith("from "))
     ]
-    main_block = up_to_first_pragma + detect_main_block(lines)
+    span, detected_main = detect_main_block(lines)
+    main_block = up_to_first_pragma + detected_main if detected_main else ""
     if main_block is not None:
         with open("__main__.py", "w") as main_file:
             # Add necessary imports
@@ -147,7 +156,37 @@ def split_file_into_module(filename):
                 main_file.write(f"from . import {name}\n")
 
             # Add the main block or call main()
-            main_file.write("\n\n" + "".join(main_block))
+            main_file.write("\n\n" + "\n".join(main_block))
         created_filenames.append("__main__.py")
+
+    # create the new files
+    for new_filename, contents in file_contents.items():
+        used_exports = exports[new_filename]
+        used_imports = parse_body_for_used_ports(contents, imports)
+
+        with open(new_filename, "w") as new_file:
+            for name in used_imports:
+                if name in imports:
+                    new_file.write(imports[name])
+            if used_exports:
+                new_file.write(f"\n__all__ = {used_exports}\n\n\n")
+
+            # hack: strip main block from the file
+            span, detected_main = detect_main_block(contents)
+            if detected_main:
+                contents = contents[: span[0] - 1] + contents[span[1] + 1 :]
+
+            new_file.writelines(contents)
+    created_filenames.extend(file_contents.keys())
+
+    # Update the __init__ file to re-export symbols from the new module directory like the old file.
+    with open("__init__.py", "w") as file:
+        for new_filename, used_exports in exports.items():
+            if used_exports:
+                module_name = re.sub(r"\.py$", "", new_filename)
+                file.write(f"from .{module_name} import *\n")
+
+        file.write(f"\n__all__ = {[x for y in exports.values() for x in y]}\n")
+    created_filenames.append("__init__.py")
 
     return created_filenames
